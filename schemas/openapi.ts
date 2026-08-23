e17a7d0483030359cd0f785dd902230f754cfd4c
pi-kari <79442987+pi-kari@users.noreply.github.com>
2026-08-23 12:37:24 +0900

    feat: update schemas

--- schemas/openapi.ts
+++ schemas/openapi.ts
@@ -1,50 +1,58 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const BookCreateSchema = z.object({
-  user_id: z.number(),
-  book_title: z.string(),
-  status: z.string(),
-  book_page: z.number()
+	user_id: z.number(),
+	book_title: z.string(),
+	status: z.string(),
+	book_page: z.number(),
 });
 
 export const BookResponseSchema = z.object({
-  book_id: z.number(),
-  book_title: z.string()
-});
-
-export const HTTPValidationErrorSchema = z.object({
-  detail: z.array(ValidationErrorSchema).optional()
-});
-
-export const ProgressResponseSchema = z.object({
-  book_id: z.number(),
-  progress: z.number(),
-  reading_history: z.union([z.object({
-    
-  }), z.array(z.unknown()), z.unknown()]).optional()
-});
-
-export const UserCreateSchema = z.object({
-  username: z.string(),
-  user_mail_address: z.union([z.string(), z.unknown()]).optional()
-});
-
-export const UserNameUpdateSchema = z.object({
-  username: z.string()
-});
-
-export const UserResponseSchema = z.object({
-  user_id: z.number(),
-  username: z.string(),
-  user_mail_address: z.union([z.string(), z.unknown()]).optional()
+	book_id: z.number(),
+	book_title: z.string(),
 });
 
 export const ValidationErrorSchema = z.object({
-  loc: z.array(z.union([z.string(), z.number()])),
-  msg: z.string(),
-  type: z.string(),
-  input: z.unknown().optional(),
-  ctx: z.object({
-    
-  }).optional()
+	loc: z.array(z.union([z.string(), z.number()])),
+	msg: z.string(),
+	type: z.string(),
+	input: z.unknown().optional(),
+	ctx: z.object({}).optional(),
+});
+
+export const HTTPValidationErrorSchema = z.object({
+	detail: z.array(ValidationErrorSchema).optional(),
+});
+
+export const ProgressResponseSchema = z.object({
+	book_id: z.number(),
+	reading_history: z.array(z.string()),
+	progress: z.number(),
+	tree_ratio: z.number(),
+	tree_state: z.number(),
+});
+
+export const ProgressUpdateSchema = z.object({
+	book_id: z.number(),
+	pages_read: z.number(),
+});
+
+export const ResponseTreeStateSchema = z.object({
+	book_id: z.number(),
+	tree_state: z.number(),
+});
+
+export const UserCreateSchema = z.object({
+	username: z.string(),
+	user_mail_address: z.union([z.string(), z.unknown()]).optional(),
+});
+
+export const UserNameUpdateSchema = z.object({
+	username: z.string(),
+});
+
+export const UserResponseSchema = z.object({
+	user_id: z.number(),
+	username: z.string(),
+	user_mail_address: z.union([z.string(), z.unknown()]).optional(),
 });
